f3e944529ec4a17a860af28ef6668b62ca06741a
Vetha-Dharshilin <merlinshylaja25@gamil.com>
2024-02-02 21:37:34 +0530

    person details

---
+++ script.js
@@ -0,0 +1,17 @@
+class person{
+
+    constructor (name,age,fathername,mothername,address,phoneno,qualification,collegename,hobbies)
+       {
+        this.name = name
+       this.age  = age
+       this.fathername = fathername
+       this.mothername = mothername
+       this.address = address
+       this.phoneno = phoneno
+       this.qualification = qualification
+       this.collegename = collegename
+           this.hobbies = hobbies
+       }
+  }
+   var p1 = new person("Mercy",21,"Vijayan","Shylaja","Thuckalay",9876543210,"B.Sc IT","NICAS","Watching Cartoon")
+   console.log(p1);
